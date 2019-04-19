a25d7598a8a57d689853fd0996a1219aa6d226b6
makimov <daggerito@gmail.com>
2019-04-19 17:48:49 +0300

    Trying to auth. Commit to migrate to mac

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from app import app
-from flask import render_template, request, jsonify
+from flask import render_template, request, flash, redirect, url_for
 import requests
+import json
 from app.forms import LoginForm
 
 
@@ -11,6 +12,9 @@ def index():
     if form.validate_on_submit():
         print("Posted data : {}".format(request.form))
         response = requests.post('https://mybook.ru/api/auth/', json={'email': request.form.get('email'), 'password': request.form.get('password')})
-        print(response)
+        print(response.json())
+        # if response.json()['password']:
+        #     flash ('Неверно введен пароль.')
+        #     return redirect(url_for('index'))
         return 'Logging in!'
     return render_template('index.html', title='Mybook mirror', form=form)
